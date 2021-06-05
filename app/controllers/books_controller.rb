b7af212b87f4bd193aa0c58d16cecd3baa9ccbd3
Mori116 <cacaomame096@gmail.com>
2021-06-05 12:10:44 +0000

    [Add]book/indexページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,19 @@
 class BooksController < ApplicationController
   def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path(@book)
   end
 
   def edit
@@ -15,6 +23,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
