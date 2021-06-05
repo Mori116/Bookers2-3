71a5e9c21a343ac3795fc76804fb19d0799daf6c
Mori116 <cacaomame096@gmail.com>
2021-06-05 03:27:40 +0000

    [Add]userコントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  resources :users, only: [:index, :show, :edit, :update]
 end
