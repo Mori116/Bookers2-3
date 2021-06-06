94840e6e7cbb85985f52d1ff605382df67b552db
Mori116 <cacaomame096@gmail.com>
2021-06-06 07:49:20 +0000

    [Finish]RSpec完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to books_path(@book)
+      redirect_to book_path(@book)
     else
       @user = current_user
       @books = Book.all
@@ -26,19 +26,18 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    if @book.user != current_user
-      redirect_to user_path(@book.user_id)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
     else
-      if @book.update(book_params)
-        flash[:notice] = "You have updated book successfully."
-        redirect_to book_path(@book)
-      else
-        render :edit
-      end
+      render :edit
     end
   end
 
