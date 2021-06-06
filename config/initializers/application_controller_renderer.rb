e292d6663a179f93c2b26fa5a0350d2236518999
Mori116 <cacaomame096@gmail.com>
2021-06-06 06:03:11 +0000

    [Add]バリデーション追記

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c27b390454b2df9c1626fee6831876ec554bc6f79a185ec7afd11d3751bdfb7a5dfb73c0765664ce80deb5a2dd8faf787aff5e2773e31568227dc10a2a826453'
