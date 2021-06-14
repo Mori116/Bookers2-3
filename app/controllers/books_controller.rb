5af0eb1238f794959e1e97baf6e59d6612728aae
Mori116 <cacaomame096@gmail.com>
2021-06-14 11:35:51 +0000

    [Add]いいねが多い順に並び替え

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,14 @@
 class BooksController < ApplicationController
   def index
     @user = current_user
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorites).
+      sort {|a,b|
+        b.favorites.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorites.includes(:favorites).where(created_at: from...to).size
+      }
     @book = Book.new
-    @books = Book.all
   end
 
   def show
